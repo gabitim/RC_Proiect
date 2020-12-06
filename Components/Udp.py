d224db29cff803b9ffbaeb4557e3071db5386fc4
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-06 16:56:04 +0200

    Fix Udp and PacketHandler port and address usage
    Remove address knowledge from Udp and PacketHandler.
    Both operate on ports only.
    Switch to socket functions that do not use addresses.

--- Components/Udp.py
+++ Components/Udp.py
@@ -11,11 +11,11 @@ SENDER_PORT = 24450
 
 
 class Udp:
-    def __init__(self, socket, source_address, destination_address=('0.0.0.0', SENDER_PORT), LOG_SIGNAL=None):
+    def __init__(self, socket, source_port, destination_port, LOG_SIGNAL=None):
         self.socket = socket
-        self.source_address = source_address
-        self.destination_address = destination_address
-        self.packet_handler = PacketHandler.PacketHandler(source_address[1], destination_address[1])
+        self.source_port = source_port
+        self.destination_port = destination_port
+        self.packet_handler = PacketHandler.PacketHandler(source_port, destination_port)
 
         self.loss_chance = 0
 
@@ -29,28 +29,24 @@ class Udp:
             self.logger.log(LogTypes.WRN, 'Packet was lost')
             return
         self.packet_handler.make(type, seq_num, data)
-        self.socket.sendto(self.packet_handler.get_bytes(), self.destination_address)
+        self.socket.sendall(self.packet_handler.get_bytes())
 
     def send_handshake(self, type, data_max_size, loss_chance, corruption_chance, filename):
         if random.randint(0, 100) <= self.LOSS_CHANCE:
             self.logger.log(LogTypes.WRN, 'Packet was lost')
             return
         self.packet_handler.make_handshake(type, data_max_size, loss_chance, corruption_chance, filename)
-        self.socket.sendto(self.packet_handler.get_bytes(), self.destination_address)
+        self.socket.sendall(self.packet_handler.get_bytes())
 
     def set_loss_chance(self, loss_chance):
         self.loss_chance = loss_chance
-
-    def set_destination_address(self, destination_address):
-        self.destination_address = destination_address
-        self.packet_handler.set_destination_port(destination_address[1])
 
     def update_buffer_size(self, buffer_size):
         self.buffer_size = buffer_size + 4
 
     # Receiving a packet with UDP
     def receive(self):
-        bytes, address = socket.recvfrom(self.buffer_size)
+        bytes = socket.recv(self.buffer_size)
         temp = self.packet_handler.unmake(bytes)
         if temp is None:
             return None
