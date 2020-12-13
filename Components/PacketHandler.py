e81d18856f1a093f26c84b628f78e02edace23ad
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-14 01:06:48 +0200

    Fix bits/bytes confusion

--- Components/PacketHandler.py
+++ Components/PacketHandler.py
@@ -7,20 +7,21 @@ from enums.packettypes import PacketTypes
 
 
 '''
-    OUR PACKET STRUCTURE
+    OUR PACKET STRUCTURE (BITS)
 --- source port [0-15]
---- destination port[16-31]
+--- destination port [16-31]
 --- length [32-47]
---- checksum [48-63]
---- type[64-66]
---- seq_num[67-95]
---- data[96..]
+--- checksum [48-79]
+--- reserved [80-92]
+--- type [93-95]
+--- seq_num [96-127]
+--- data[128..]
 '''
 
-
+# THE REST OF THIS CLASS USES BYTES ONLY!!!
 class PacketHandler:
-    HEADER_SIZE = 96
-    HANDSHAKE_SIZE = 64
+    HEADER_SIZE = 16
+    HANDSHAKE_SIZE = 10
 
     def __init__(self, source_port, destination_port, CORRUPTION_CHANCE, LOG_SIGNAL=None):
         self.source_port = source_port
@@ -46,10 +47,10 @@ class PacketHandler:
     def make_handshake(self, type, data_max_size, loss_chance, corruption_chance, filename):
         self.type = type
         self.seq_num = 0
-        self.data = data_max_size.to_bytes(16, 'little')
-        self.data += loss_chance.to_bytes(8, 'little')
-        self.data += corruption_chance.to_bytes(8, 'little')
-        self.data += filename.encode().ljust(32, b'\0')
+        self.data = data_max_size.to_bytes(2, 'little')
+        self.data += loss_chance.to_bytes(2, 'little')
+        self.data += corruption_chance.to_bytes(2, 'little')
+        self.data += filename.encode().ljust(4, b'\0')
 
         self.compute_length()
         self.compute_checksum()
@@ -59,18 +60,22 @@ class PacketHandler:
 
     def compute_checksum(self):
         packet_bytes = self.get_bytes()
-        self.checksum = zlib.crc32(packet_bytes[:48] + packet_bytes[64:])
+        self.checksum = zlib.crc32(packet_bytes[:6] + packet_bytes[10:])
 
     def get_type(self):
         return self.type
 
     def get_bytes(self):
-        packet_bytes = self.source_port.to_bytes(16, 'little')
-        packet_bytes += self.destination_port.to_bytes(16, 'little')
-        packet_bytes += self.length.to_bytes(16, 'little')
-        packet_bytes += self.checksum.to_bytes(16, 'little')
-        packet_bytes += self.type.to_bytes(3, 'little')
-        packet_bytes += self.seq_num.to_bytes(29, 'little', signed=True)
+        packet_bytes = self.source_port.to_bytes(2, 'little')
+        packet_bytes += self.destination_port.to_bytes(2, 'little')
+        packet_bytes += self.length.to_bytes(2, 'little')
+        packet_bytes += self.checksum.to_bytes(4, 'little')
+
+        # reserved is not currently planned for use
+        reserved = 0
+        reserved_and_type = (reserved << 3) | self.type.value
+        packet_bytes += reserved_and_type.to_bytes(2, 'little')
+        packet_bytes += self.seq_num.to_bytes(4, 'little', signed=True)
         packet_bytes += self.data
 
         ''' simulate wrong checksum!! '''
@@ -84,13 +89,13 @@ class PacketHandler:
 
     def decode(self, packet_bytes):
         # decode up to checksum
-        source_port = int.from_bytes(packet_bytes[0:16], 'little')
-        destination_port = int.from_bytes(packet_bytes[16:32], 'little')
-        length = int.from_bytes(packet_bytes[32:48], 'little')
-        checksum = int.from_bytes(packet_bytes[48:64], 'little')
+        source_port = int.from_bytes(packet_bytes[0:2], 'little')
+        destination_port = int.from_bytes(packet_bytes[2:4], 'little')
+        length = int.from_bytes(packet_bytes[4:6], 'little')
+        checksum = int.from_bytes(packet_bytes[6:10], 'little')
 
         # Check Packet Integrity
-        if checksum != zlib.crc32(packet_bytes[:48] + packet_bytes[64:]):
+        if checksum != zlib.crc32(packet_bytes[:6] + packet_bytes[10:]):
             self.logger.log(LogTypes.WRN, 'Packet was corrupted')
             return None
 
@@ -109,9 +114,11 @@ class PacketHandler:
             return None
 
         # decode after checksum
-        type = PacketTypes(int.from_bytes(packet_bytes[64:67], 'little'))
-        seq_num = int.from_bytes(packet_bytes[67:96], 'little', signed=True)
-        data = packet_bytes[96:]
+        reserved_and_type = int.from_bytes(packet_bytes[10:12], 'little')
+        reserved = reserved_and_type >> 3
+        type = PacketTypes(reserved_and_type & 7) # first 3 bits
+        seq_num = int.from_bytes(packet_bytes[12:16], 'little', signed=True)
+        data = packet_bytes[16:]
 
         return type, seq_num, data
 
@@ -126,10 +133,10 @@ class PacketHandler:
             return temp
 
     def unmake_handshake(self, data):
-        data_max_size = int.from_bytes(data[0:16], 'little')
-        loss_chance = int.from_bytes(data[16:24], 'little')
-        corruption_chance = int.from_bytes(data[24:32], 'little')
-        filename = data[32:].rstrip(b'\0').decode()
+        data_max_size = int.from_bytes(data[0:2], 'little')
+        loss_chance = int.from_bytes(data[2:4], 'little')
+        corruption_chance = int.from_bytes(data[4:6], 'little')
+        filename = data[6:].rstrip(b'\0').decode()
 
         self.set_corruption_chance(corruption_chance)
         return data_max_size, loss_chance, filename
